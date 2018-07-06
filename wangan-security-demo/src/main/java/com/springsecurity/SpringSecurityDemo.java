0eb89af109e9d447031157fbb56cae364bcda7cc
wa <15155313869@163.com>
2018-07-06 17:15:34 +0800

    login page

--- wangan-security-demo/src/main/java/com/springsecurity/SpringSecurityDemo.java
+++ wangan-security-demo/src/main/java/com/springsecurity/SpringSecurityDemo.java
@@ -8,6 +8,7 @@ public class SpringSecurityDemo {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityDemo.class,args);
+        System.out.println("test");
     }
 
 }
